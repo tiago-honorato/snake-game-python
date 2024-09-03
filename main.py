e5c10dd51f36553271d31aee2103dfe37104576e
Tiago <tiagohirute@gmail.com>
2024-09-03 00:20:20 -0300

    collision added

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ import pygame.locals
 WINDOW_SIZE = (600, 600);
 PIXEL_SIZE = 10
 
+def collision(pos1, pos2):
+    return pos1 == pos2
+
+def off_limits(pos):
+    if 0 <= pos[0] < WINDOW_SIZE[0] and 0 <= pos[1] < WINDOW_SIZE[1]:
+        return False
+    else:
+        return True
+
 pygame.init();
 screen = pygame.display.set_mode(WINDOW_SIZE);
 
@@ -13,6 +22,7 @@ pygame.display.set_caption("Snake Game");
 snake_pos = [(250, 50), (260, 50), (270, 50)]
 snake_surface = pygame.Surface((PIXEL_SIZE, PIXEL_SIZE))
 snake_surface.fill((255, 255, 255))
+snake_direction = K_LEFT
 
 apple_surface = pygame.Surface((PIXEL_SIZE, PIXEL_SIZE))
 apple_surface.fill((255, 0, 0))
@@ -25,9 +35,31 @@ while(True):
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
+
+        elif event.type == KEYDOWN:
+            if event.key in [K_UP, K_DOWN, K_LEFT, K_RIGHT]:
+                snake_direction = event.key
+
     for pos in snake_pos:
         screen.blit(snake_surface, pos)
 
-    snake_pos[0] = snake_pos[0][0] + 10, snake_pos[0][1]
+    for i in range(len(snake_pos)-1, 0, -1):
+        if collision(snake_pos[0], snake_pos[i]):
+            pygame.quit()
+            quit()
+        snake_pos[i] = snake_pos[i-1]
+
+    if off_limits(snake_pos[0]):
+        pygame.quit()
+        quit()
+
+    if snake_direction == K_UP:
+        snake_pos[0] = (snake_pos[0][0], snake_pos[0][1] - PIXEL_SIZE)
+    elif snake_direction == K_DOWN:
+        snake_pos[0] = (snake_pos[0][0], snake_pos[0][1] + PIXEL_SIZE)
+    elif snake_direction == K_LEFT:
+        snake_pos[0] = (snake_pos[0][0] - PIXEL_SIZE, snake_pos[0][1])
+    elif snake_direction == K_RIGHT:
+        snake_pos[0] = (snake_pos[0][0] + PIXEL_SIZE, snake_pos[0][1])
     
     pygame.display.update()
